2693744cf65a3e17fabee34e478a07c2309234d2
bosgood <bosgood@gmail.com>
2012-04-26 01:37:13 -0400

    added different tables for different races

--- racersTable.coffee
+++ racersTable.coffee
@@ -1,14 +1,19 @@
 google.load 'visualization', '1', (packages:['table'])
+google.load 'jquery', '1.7.1'
 google.setOnLoadCallback () ->
-    tableDiv = document.getElementById 'racers_table'
-    if (tableDiv == null)
-        return
-    sql = encodeURIComponent "SELECT Value as Place, Name FROM 3433619 WHERE Event = 'race' ORDER BY Value ASC"
-    url = "http://www.google.com/fusiontables/gvizdata?tq=#{sql}"
-    query = new google.visualization.Query(url)
-    query.send (response) ->
-        if response.isError()
+    tables = document.getElementsByClassName 'blackfalcon-top10'
+    for tableDiv in tables
+        raceData = $(tableDiv).data('blackfalcon')
+        if (tableDiv == null)
             return
-        data = response.getDataTable()
-        table = new google.visualization.Table(tableDiv)
-        table.draw data, (showRowNumber: false, alternatingRowStyle: false)
+        sql = "SELECT Value as Place, Name FROM 3433619 WHERE Event = '#{raceData.eventType}' AND RaceNum = #{raceData.race} ORDER BY Value ASC"
+        console.log sql
+        sql = encodeURIComponent sql
+        url = "http://www.google.com/fusiontables/gvizdata?tq=#{sql}"
+        query = new google.visualization.Query(url)
+        query.send (response) ->
+            if response.isError()
+                return
+            data = response.getDataTable()
+            table = new google.visualization.Table(tableDiv)
+            table.draw data, (showRowNumber: false, alternatingRowStyle: false)
--- racersTable.js
+++ racersTable.js
@@ -1,1 +1,34 @@
-((function(){google.load("visualization","1",{packages:["table"]});google.setOnLoadCallback(function(){var a,b,c,d;c=document.getElementById("racers_table");if(c===null)return;b=encodeURIComponent("SELECT Value as Place, Name FROM 3433619 WHERE Event = 'race' ORDER BY Value ASC");d="http://www.google.com/fusiontables/gvizdata?tq="+b;a=new google.visualization.Query(d);return a.send(function(a){var b,d;if(a.isError())return;b=a.getDataTable();d=new google.visualization.Table(c);return d.draw(b,{showRowNumber:!1,alternatingRowStyle:!1})})})})).call(this);
+(function() {
+
+  google.load('visualization', '1', {
+    packages: ['table']
+  });
+
+  google.load('jquery', '1.7.1');
+
+  google.setOnLoadCallback(function() {
+    var query, raceData, sql, tableDiv, tables, url, _i, _len;
+    tables = document.getElementsByClassName('blackfalcon-top10');
+    for (_i = 0, _len = tables.length; _i < _len; _i++) {
+      tableDiv = tables[_i];
+      raceData = $(tableDiv).data('blackfalcon');
+      if (tableDiv === null) return;
+      sql = "SELECT Value as Place, Name FROM 3433619 WHERE Event = '" + raceData.eventType + "' AND RaceNum = " + raceData.race + " ORDER BY Value ASC";
+      console.log(sql);
+      sql = encodeURIComponent(sql);
+      url = "http://www.google.com/fusiontables/gvizdata?tq=" + sql;
+      query = new google.visualization.Query(url);
+      query.send(function(response) {
+        var data, table;
+        if (response.isError()) return;
+        data = response.getDataTable();
+        table = new google.visualization.Table(tableDiv);
+        return table.draw(data, {
+          showRowNumber: false,
+          alternatingRowStyle: false
+        });
+      });
+    }
+  });
+
+}).call(this);
